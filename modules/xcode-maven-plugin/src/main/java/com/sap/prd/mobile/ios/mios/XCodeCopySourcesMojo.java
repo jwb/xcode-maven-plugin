9b981605126e780fcb054ca30ae39af078c2c09f
Tracy Keeling <tracyk@zillow.com>
2013-05-28 12:01:59 -0700

    Use rsync instead of Java FileUtils so that we can properly copy symlinks, permissions, etc.

--- modules/xcode-maven-plugin/src/main/java/com/sap/prd/mobile/ios/mios/XCodeCopySourcesMojo.java
+++ modules/xcode-maven-plugin/src/main/java/com/sap/prd/mobile/ios/mios/XCodeCopySourcesMojo.java
@@ -29,94 +29,99 @@ import org.apache.maven.plugin.MojoFailureException;
 
 /**
  * Copies sources to the checkout directory below the target directory.
- * 
+ *
  * @goal copy-sources
  */
 public class XCodeCopySourcesMojo extends AbstractXCodeMojo
 {
 
-  @Override
-  public void execute() throws MojoExecutionException, MojoFailureException
-  {    
-    final File baseDirectory = project.getBasedir();
-    final File checkoutDirectory = getCheckoutDirectory();
-    final String buildDirPath = getProjectBuildDirectory();
-    
-    getLog().info("Base directory: " + baseDirectory);
-    getLog().info("Checkout directory: " + checkoutDirectory);
-    getLog().info("BuildDirPath: " + buildDirPath);
-    
-    
-    final File originalLibDir = new File(project.getBuild().getDirectory(), FolderLayout.LIBS_DIR_NAME);
-    final File copyOfLibDir = new File(checkoutDirectory, buildDirPath + "/" + FolderLayout.LIBS_DIR_NAME);
-    
-    final File originalHeadersDir = new File(project.getBuild().getDirectory(), FolderLayout.HEADERS_DIR_NAME);
-    final File copyOfHeadersDir = new File(checkoutDirectory, buildDirPath + "/" + FolderLayout.HEADERS_DIR_NAME);
-    
-    final File originalXcodeDepsDir = new File(project.getBuild().getDirectory(), FolderLayout.XCODE_DEPS_TARGET_FOLDER);
-    final File copyOfXcodeDepsDir = new File(checkoutDirectory, buildDirPath + "/" + FolderLayout.XCODE_DEPS_TARGET_FOLDER);
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
+        final File baseDirectory = project.getBasedir();
+        final File checkoutDirectory = getCheckoutDirectory();
+        final String buildDirPath = getProjectBuildDirectory();
 
-    try {
+        getLog().info("Base directory: " + baseDirectory);
+        getLog().info("Checkout directory: " + checkoutDirectory);
+        getLog().info("BuildDirPath: " + buildDirPath);
 
-      if (checkoutDirectory.exists())
-        com.sap.prd.mobile.ios.mios.FileUtils.deleteDirectory(checkoutDirectory);
 
-      copy(baseDirectory, checkoutDirectory, new FileFilter() {
+        final File originalLibDir = new File(project.getBuild().getDirectory(), FolderLayout.LIBS_DIR_NAME);
+        final File copyOfLibDir = new File(checkoutDirectory, buildDirPath + "/" + FolderLayout.LIBS_DIR_NAME);
 
-        @Override
-        public boolean accept(File pathname)
-        {
-          return ! (checkoutDirectory.getAbsoluteFile().equals(pathname.getAbsoluteFile()) || 
-                    originalLibDir.getAbsoluteFile().equals(pathname.getAbsoluteFile()) ||
-                    originalHeadersDir.getAbsoluteFile().equals(pathname.getAbsoluteFile()) || 
-                    originalXcodeDepsDir.getAbsoluteFile().equals(pathname.getAbsoluteFile()));
+        final File originalHeadersDir = new File(project.getBuild().getDirectory(), FolderLayout.HEADERS_DIR_NAME);
+        final File copyOfHeadersDir = new File(checkoutDirectory, buildDirPath + "/" + FolderLayout.HEADERS_DIR_NAME);
+
+        final File originalXcodeDepsDir = new File(project.getBuild().getDirectory(), FolderLayout.XCODE_DEPS_TARGET_FOLDER);
+        final File copyOfXcodeDepsDir = new File(checkoutDirectory, buildDirPath + "/" + FolderLayout.XCODE_DEPS_TARGET_FOLDER);
+
+        try {
+
+            if (checkoutDirectory.exists())
+                com.sap.prd.mobile.ios.mios.FileUtils.deleteDirectory(checkoutDirectory);
+
+            int returnValue = Forker.forkProcess(
+                    System.out,
+                    null,
+                    "rsync",
+                    "--recursive",
+                    "--perms",
+                    "--executability",
+                    "--links",
+                    "--safe-links",
+                    "--exclude",
+                    "'/" + buildDirPath + "'",
+                    baseDirectory.getAbsolutePath() + "/",
+                    checkoutDirectory.getAbsolutePath());
+            if (returnValue != 0) {
+                throw new RuntimeException("Could not copy '" + baseDirectory + "' to '"  + checkoutDirectory + "'. Return value:" + returnValue);
+            }
+
+
+            if(originalLibDir.exists())
+                com.sap.prd.mobile.ios.mios.FileUtils.createSymbolicLink(originalLibDir, copyOfLibDir);
+
+            if(originalHeadersDir.exists())
+                com.sap.prd.mobile.ios.mios.FileUtils.createSymbolicLink(originalHeadersDir, copyOfHeadersDir);
+
+            if(originalXcodeDepsDir.exists())
+                com.sap.prd.mobile.ios.mios.FileUtils.createSymbolicLink(originalXcodeDepsDir, copyOfXcodeDepsDir);
+
         }
-        
-      });
-
-      if(originalLibDir.exists())
-        com.sap.prd.mobile.ios.mios.FileUtils.createSymbolicLink(originalLibDir, copyOfLibDir);
-      
-      if(originalHeadersDir.exists())
-        com.sap.prd.mobile.ios.mios.FileUtils.createSymbolicLink(originalHeadersDir, copyOfHeadersDir);
-      
-      if(originalXcodeDepsDir.exists())
-        com.sap.prd.mobile.ios.mios.FileUtils.createSymbolicLink(originalXcodeDepsDir, copyOfXcodeDepsDir);
-       
+        catch (IOException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
     }
-    catch (IOException e) {
-      throw new MojoExecutionException(e.getMessage(), e);
+
+    /**
+     // Return the part of the path between project base directory and project build directory.
+     // Assumption is: project build directory is located below project base directory.
+     **/
+    private String getProjectBuildDirectory() {
+        return com.sap.prd.mobile.ios.mios.FileUtils.getDelta(project.getBasedir(), new File(project.getBuild().getDirectory()));
     }
-  }
+    private void copy(final File source, final File targetDirectory, final FileFilter excludes) throws IOException
+    {
 
-  /**
-  // Return the part of the path between project base directory and project build directory.
-  // Assumption is: project build directory is located below project base directory. 
-  **/
-  private String getProjectBuildDirectory() {
-    return com.sap.prd.mobile.ios.mios.FileUtils.getDelta(project.getBasedir(), new File(project.getBuild().getDirectory()));
-  }
-  private void copy(final File source, final File targetDirectory, final FileFilter excludes) throws IOException
-  {
+        for (final File sourceFile : source.listFiles()) {
+            final File destFile = new File(targetDirectory, sourceFile.getName());
+            if (sourceFile.isDirectory()) {
 
-    for (final File sourceFile : source.listFiles()) {
-      final File destFile = new File(targetDirectory, sourceFile.getName());
-      if (sourceFile.isDirectory()) {
-        
-        if (excludes.accept(sourceFile)) {
-          copy(sourceFile, destFile, excludes);
-        }
-        else {
-          getLog().info("File '" + sourceFile + "' ommited.");
-        }
-      }
-      else {
+                if (excludes.accept(sourceFile)) {
+                    copy(sourceFile, destFile, excludes);
+                }
+                else {
+                    getLog().info("File '" + sourceFile + "' ommited.");
+                }
+            }
+            else {
         FileUtils.copyFile(sourceFile, destFile);
         if (sourceFile.canExecute()) {
             destFile.setExecutable(true);
-        }
+                }
         getLog().debug((destFile.canExecute() ? "Executable" : "File '") + sourceFile + "' copied to '" + destFile + "'.");
-      }
+            }
+        }
     }
-  }
 }
